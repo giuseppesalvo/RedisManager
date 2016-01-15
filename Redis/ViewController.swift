c9320cf2fc8e99a8872311316329dcec7539ca96
giuseppesalvo <giuseppesalvo@outlook.it>
2016-01-15 12:55:09 +0100

    Initial Commit

---
+++ Redis/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Redis
+//
+//  Created by Seth on 15/01/16.
+//  Copyright © 2016 Seth. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
